2e054d2009b7266e6d77a9246c7db4db77c438da
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-12-27 12:17:49 +0200

    message

--- src/features/register/ValidationSchema/ValidationSchema.jsx
+++ src/features/register/ValidationSchema/ValidationSchema.jsx
@@ -1,24 +1,34 @@
 import * as yup from "yup";
 
+const addressRules = /\d/;
+
 export const registerSchema = yup.object().shape({
   firstname: yup
     .string()
     .min(2, "Username must be at least 2 characters long")
+    .max(120, "Username is too long")
     .required("Required"),
   lastname: yup
     .string()
-    .min(2, "Username must be at least 2 characters long")
+    .min(2, "Lastname must be at least 2 characters long")
+    .max(120, "Lastname is too long")
     .required("Required"),
   country: yup
     .string()
     .oneOf(["romania", "italia", "spania", "germania"], "Invalid Country")
     .required("Required"),
-  address: yup.string().min(20, "Street, number, etc ...").required("Required"),
+  address: yup
+    .string()
+    .min(10, "Street, number, etc ...")
+    .max(50, "Address is too long")
+    .matches(addressRules, { message: "Invalid Address" })
+    .required("Required"),
   age: yup
     .number()
     .positive()
     .integer()
     .min(18, "You are not old enough")
+    .max(100, "Invalid age")
     .required("Required"),
   gender: yup.string().required("Required"),
 
