90e0114b287202b4386f8c784779e5c9c3270786
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-12-27 14:41:27 +0200

    local storage try

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./state-management/store";
+import { store, persistor } from "./state-management/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <React.StrictMode>
       <Provider store={store}>
+        {/* <PersistGate persistor={persistor}> */}
         <BrowserRouter>
           <App />
         </BrowserRouter>
+        {/* </PersistGate> */}
       </Provider>
     </React.StrictMode>
   </>
--- src/state-management/store.js
+++ src/state-management/store.js
@@ -2,7 +2,34 @@ import { applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// varianta cu logger
 
 const createStoreWithMiddlerware = applyMiddleware(logger)(configureStore);
 
 export const store = createStoreWithMiddlerware({ reducer: rootReducer });
+
+// varianta cu logger si local storage care momentan functioneaza doar local storage nu si logger
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist: ["login", "_persist"],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const createStoreWithMiddlerware = applyMiddleware(logger);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }),
+//   logger,
+// });
+
+// export const persistor = persistStore(store);
