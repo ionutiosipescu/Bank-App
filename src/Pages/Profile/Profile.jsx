b3b7f58c5da0d29515927ea64006dc7ad28697a7
Alex Aandonesei <109587363+AlexCodeTitan@users.noreply.github.com>
2022-12-31 10:52:42 +0200

    card update & profile updates

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import ProfileComponent from "../../components/ProfileComponent/ProfileComponent";
 import { TopContainer } from "../Cards/Cards.style";
-import { PlanContainer } from "./Profile.style";
+import { PlanContainer, UsersContainer, UsersWrapper } from "./Profile.style";
+
+import PlanCard from "../../components/PlanCard/PlanCard";
+import UserCard from "../../components/UserCard/UserCard";
+import { SettingsContainer } from "../Settings/Settings.style";
 
 import { accounts } from "../../utils/data/dummyData";
-import PlanCard from "../../components/PlanCard/PlanCard";
 
 function Profile() {
   return (
-    <TopContainer>
-      <ProfileComponent
-        name={accounts[0].owner}
-        address={accounts[0].address}
-        email={accounts[0].email}
-        phoneNumber={accounts[0].phoneNumber}
-      ></ProfileComponent>
-      <PlanContainer>
-        <PlanCard />
-      </PlanContainer>
-    </TopContainer>
+    <>
+      <TopContainer>
+        <ProfileComponent
+          name={accounts[0].owner}
+          address={accounts[0].address}
+          email={accounts[0].email}
+          phoneNumber={accounts[0].phoneNumber}
+        ></ProfileComponent>
+        <PlanContainer>
+          <PlanCard />
+        </PlanContainer>
+      </TopContainer>
+      <SettingsContainer>
+        <UsersWrapper>
+          <h2>Recent Users</h2>
+          <UsersContainer>
+            {accounts.map((account) => (
+              <UserCard
+                name={account.owner}
+                email={account.email}
+                phone={account.phoneNumber}
+                image={account.image}
+                to="/dashboard"
+              />
+            ))}
+          </UsersContainer>
+        </UsersWrapper>
+      </SettingsContainer>
+    </>
   );
 }
 
