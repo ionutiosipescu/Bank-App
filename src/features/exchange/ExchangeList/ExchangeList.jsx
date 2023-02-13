11d0d4c4adea407fee4b84aec7776155e24ad06b
ionutiosipescu <ionutiosipescu@yahoo.com>
2023-02-13 17:16:02 +0200

    exchange

--- src/features/exchange/ExchangeList/ExchangeList.jsx
+++ src/features/exchange/ExchangeList/ExchangeList.jsx
@@ -16,18 +16,21 @@ import {
   RateContainer,
 } from "./ExchangeList.style";
 import Button from "../../../components/UI/Button/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectExchangeArr } from "../../../state-management/Dashboard/services/helpers/exchangeHelper/exchangeHelper.selector";
+import { fetchExchangeRepeat } from "../../../state-management/Dashboard/services/helpers/exchangeHelper/exchangeHelper.action";
+import { selectCurrentUser } from "../../../state-management/Dashboard/userData/userData.selector";
 
 function ExchangeList() {
-  // transfer.amountToObj = amount converted
+  const dispatch = useDispatch();
   const exchangeArr = useSelector(selectExchangeArr);
-
+  const currentUser = useSelector(selectCurrentUser);
   const setExchangeArr = (currency) => {
     const string =
       currency === "RonToEuro" ? `RON \u{2194} EURO` : `EURO \u{2194} RON`;
     return string;
   };
+
   return (
     <ServiceCard>
       <CardHeader>
@@ -44,7 +47,16 @@ function ExchangeList() {
               <AmountContainer>Sum: {transfer.exchange}</AmountContainer>
               <DateContainer>{transfer.date}</DateContainer>
             </ListItemSection>
-            <Button label="Repeat" size="sm" primary="primary" />
+            <Button
+              label="Repeat"
+              size="sm"
+              primary="primary"
+              onClick={() =>
+                dispatch(
+                  fetchExchangeRepeat(transfer, exchangeArr, currentUser)
+                )
+              }
+            />
           </ListItem>
         ))}
       </ListContainer>
--- src/state-management/Dashboard/services/helpers/exchangeHelper/exchangeHelper.action.js
+++ src/state-management/Dashboard/services/helpers/exchangeHelper/exchangeHelper.action.js
@@ -3,6 +3,7 @@ import { getLocalDate } from "../../../../../utils/helpers/helperFunctions/date"
 import { EXCHANGE_HELPER_TYPES } from "./excahngeHelper.types";
 import { findObjectByString } from "../../../../../utils/helpers/helperFunctions/findObject";
 import axios from "axios";
+import { generateRandomNumber } from "../../../../../utils/helpers/helperFunctions/randomNumber";
 
 export const updateExhangeAmount = (exchangeData, e) => {
   const { name, value } = e.target;
@@ -22,7 +23,7 @@ export const setExchangeAmountTo = (exchangeData, amountTo) => {
   );
 };
 
-const setExchangeData = (obj) => {
+const setExchangeData = async (obj) => {
   const { amount } = obj;
   const date = getLocalDate();
   const cleanExchangeData = {
@@ -61,12 +62,14 @@ export const setExchangeArrDb = (exchangeArr) => {
 
 export const setExchangeArr = (obj, arr) => {
   const { amount, currency } = obj;
-  const string = currency === "ron" ? `RON \u{2194} EURO` : `EURO \u{2194} RON`;
+  const string = currency === "ron" ? `RonToEuro` : `EuroToRon`;
+  console.log(string);
   const objArr = {
-    detail: string,
-    rate: "5.23",
-    amount: amount,
+    id: generateRandomNumber(3).toString(),
+    type_exchange: string,
+    exchange: amount,
     date: getLocalDate(),
+    details: "",
   };
   const newExchangeArr = [...arr, { ...objArr }];
   return createAction(EXCHANGE_HELPER_TYPES.SET_EXCHANGE_ARR, newExchangeArr);
@@ -74,10 +77,54 @@ export const setExchangeArr = (obj, arr) => {
 
 export const fetchExchangeData = (obj, arr, currentUserData) => {
   return async (dispatch) => {
+    console.log(obj, arr, currentUserData);
     await dispatch(setExchangeArr(obj, arr));
     try {
       const exchangeData = await setExchangeData(obj);
       const id = await setExchangeId(obj, currentUserData);
+      await axios
+        .post(`http://localhost:8080/exchanges/exchange?id=${id}`, exchangeData)
+        .then((res) => console.log(res));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+// REPEAT
+
+// refactor in future DRY principale
+export const setExchangeArrRepeat = (obj, arr) => {
+  const newExchangeArr = [...arr, { ...obj }];
+  return createAction(EXCHANGE_HELPER_TYPES.SET_EXCHANGE_ARR, newExchangeArr);
+};
+
+const setExchangeDataRepeat = async (obj) => {
+  const { exchange } = obj;
+  const date = getLocalDate();
+  const cleanExchangeData = {
+    exchange: Number(exchange),
+    details: "",
+    date: date,
+  };
+  return cleanExchangeData;
+};
+
+export const setExchangeIdRepeat = (obj, currentUserData) => {
+  const { type_exchange } = obj;
+  const currency = type_exchange === "RonToEuro" ? "ron" : "euro";
+  const stringCompare = "currency";
+  const userAccountArr = currentUserData.account;
+  const object = findObjectByString(currency, userAccountArr, stringCompare);
+  return object.id;
+};
+
+export const fetchExchangeRepeat = (obj, arr, currentUserData) => {
+  return async (dispatch) => {
+    await dispatch(setExchangeArrRepeat(obj, arr));
+    try {
+      const exchangeData = await setExchangeDataRepeat(obj);
+      const id = await setExchangeIdRepeat(obj, currentUserData);
       await axios
         .post(`http://localhost:8080/exchanges/exchange?id=${id}`, exchangeData)
         .then((res) => console.log(res));
