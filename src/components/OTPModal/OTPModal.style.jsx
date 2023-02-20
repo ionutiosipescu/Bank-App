18f23af389e66399aaadb216d1c1fdace1f0e776
Alex <aandoneseialex@gmail.com>
2023-02-20 18:07:56 +0200

    OTP Input

--- src/components/OTPModal/OTPModal.style.jsx
+++ src/components/OTPModal/OTPModal.style.jsx
@@ -11,9 +11,19 @@ export const OTPContainer = styled.div`
 
 export const InputsContainer = styled.div`
   display: flex;
-  width: 100%;
+  width: fit-content;
   justify-content: center;
   align-items: center;
+  & input {
+    height: 50px;
+    margin: 0 5px 0 5px;
+    text-align: center;
+    font-size: 1.6rem;
+    border-radius: 10px;
+    outline: none;
+    border: 1px solid var(--purple-dark);
+    color: var(--text);
+  }
 `;
 
 export const OTPInput = styled.input.attrs({ type: "tel" })`
@@ -35,4 +45,28 @@ export const OTPInput = styled.input.attrs({ type: "tel" })`
         margin: 0;
       }
     `};
+`;
+
+export const OTPInput_2 = styled.input.attrs({
+  type: "tel",
+})`
+  display: block;
+  margin: 2rem auto;
+  border: none;
+  padding: 0;
+  width: 260px;
+  background: repeating-linear-gradient(
+      90deg,
+      dimgrey 0,
+      dimgrey 1.6rem,
+      transparent 0,
+      transparent 2.8rem
+    )
+    0 100%/ 16rem 2px no-repeat;
+  font: 3rem droid sans moro, consolas, monospace;
+  letter-spacing: 1.15rem;
+  &:focus {
+    outline: none;
+    color: var(--purple);
+  }
 `;
